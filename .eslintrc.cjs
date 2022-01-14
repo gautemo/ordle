1dd3780af1b42af4d3f2aa00ff8627c32451f8e0
Gaute Meek Olsen <gaute.meek@gmail.com>
2022-01-14 20:05:39 +0100

    init vue project

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript/recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  env: {
+    "vue/setup-compiler-macros": true,
+  },
+};
