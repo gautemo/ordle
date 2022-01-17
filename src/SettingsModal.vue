758b3698a7273a59f01b5609158f78aabdf54136
Gaute Meek Olsen <gaute.meek@gmail.com>
2022-01-17 20:13:21 +0100

    settings checkbox

--- src/SettingsModal.vue
+++ src/SettingsModal.vue
@@ -6,11 +6,39 @@ const isDark = useDark()
 
 <template>
   <section>
-    <p>Nattmodus</p>
-    <input type="checkbox" v-model="isDark" />
+    <label>
+      <input type="checkbox" v-model="isDark" />
+      <span>Nattmodus</span>
+    </label>
   </section>
 </template>
 
 <style scoped>
+label{
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: .5rem;
+  font-size: 1.5rem;
+}
 
+input{
+  position: relative;
+  appearance: none;
+  width: 2em;
+  height: 2em;
+  border: 0.15em solid currentColor;
+  border-radius: 0.2em;
+}
+input::before{
+  content: "";
+  position: absolute;
+  inset: 2px;
+  background-color: var(--correct);
+  transform: scale(0);
+  transition: 250ms transform ease-in-out;
+}
+input:checked::before{
+  transform: scale(1);
+}
 </style>
