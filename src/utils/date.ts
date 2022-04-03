03e6cc22366338cde3dd23250e9a1c3a909afadf
Gaute Meek Olsen <gaute.meek@gmail.com>
2022-04-03 04:28:19 +0200

    fix: days since start now fixes issue with timezone possible beind slightly off between dates

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,21 @@
-  export const datesAreOnSameDay = (first: Date, second: Date) => {
-    return first.getFullYear() === second.getFullYear() &&
-      first.getMonth() === second.getMonth() &&
-      first.getDate() === second.getDate()
-  }
+export const datesAreOnSameDay = (first: Date, second: Date) => {
+  return first.getFullYear() === second.getFullYear() &&
+    first.getMonth() === second.getMonth() &&
+    first.getDate() === second.getDate()
+}
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest
+
+  it('dates should be same day', () => {
+    const date1 = new Date(2022, 2, 3, 0, 0, 0)
+    const date2 = new Date(2022, 2, 3, 0, 0, 1)
+    expect(datesAreOnSameDay(date1, date2)).toEqual(true)
+  })
+
+  it('dates should be not be same day', () => {
+    const date1 = new Date(2022, 2, 3, 0, 0, 0)
+    const date2 = new Date(2022, 2, 2, 23, 59, 59)
+    expect(datesAreOnSameDay(date1, date2)).toEqual(false)
+  })
+}
