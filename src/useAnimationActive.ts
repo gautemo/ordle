885ca5b380e7853bdb9bff8a21818136adbe2bb4
Gaute Meek Olsen <gaute.meek@gmail.com>
2023-01-17 19:45:27 +0100

    fix: Safari v16.2 transform bug

--- src/useAnimationActive.ts
+++ src/useAnimationActive.ts
@@ -1,10 +1,12 @@
 import { useStorage } from '@vueuse/core'
 import { watchEffect } from 'vue'
 
+const isSafariv16_2 = navigator.userAgent.includes('Version/16.2 Safari')
+
 export const animationOff = useStorage('reduce-animation', false)
 
 export function useAnimationActive() {
   watchEffect(() => {
-    document.documentElement.classList.toggle('reduce-animation', animationOff.value)
+    document.documentElement.classList.toggle('reduce-animation', animationOff.value || isSafariv16_2)
   })
 }
