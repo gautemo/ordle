7b3d282d9895f42ab4a9bbe213d4744fba6f6a6b
Gaute Meek Olsen <gaute.meek@gmail.com>
2023-01-20 14:15:29 +0100

    fix safari v16.2 check

--- src/useAnimationActive.ts
+++ src/useAnimationActive.ts
@@ -1,7 +1,7 @@
 import { useStorage } from '@vueuse/core'
 import { watchEffect } from 'vue'
 
-export const isSafariv16_2 = navigator.userAgent.includes('Version/16.2 Safari')
+export const isSafariv16_2 = navigator.userAgent.includes('Version/16.2 Safari') || navigator.userAgent.includes('iPhone OS 16_2')
 
 export const animationOff = useStorage('reduce-animation', false)
 
